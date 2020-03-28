83366ef8df107bb318ad75183801d9c2f1a0e71e
Ewan Watt <watt.ewan99@gmail.com>
2020-03-28 16:30:28 -0700

    Changing kahfka to dns name

--- Data Storage/create_tables_mysql.py
+++ Data Storage/create_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="localhost", user="root", password="P@ssw0rd", database="events")
+db_conn = mysql.connector.connect(host="cloudserver-acit3855.westus.cloudapp.azure.com", user="root", password="root", database="events")
 
 
 db_cursor = db_conn.cursor()
